69fa17ba2b4d2ca8e3c891486d9ed93a59f24af1
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-10 17:53:36 +0900

    2023-01-10. troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import TOC from "./components/TOC";
 import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    };
+  }
+
   render() {
     return( 
       <div className = "App" >
         Hello react!
-        <Subject title="WEB" content="world wide web"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          content={this.state.subject.sub}></Subject>
+        <TOC data={this.state.content}></TOC>
         <Subject title="REACT" content="react!"></Subject>
       </div>
     );
