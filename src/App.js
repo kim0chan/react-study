a6492fee0f12f170a2715e8531fce6c5d117a9d1
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-12 22:48:48 +0900

    lecture 16

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -20,14 +20,24 @@ class App extends Component {
   }
 
   render() {
-    return( 
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    
+    return(
       <div className = "App" >
         Hello react!
         <Subject
           title={this.state.subject.title}
           content={this.state.subject.sub}></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is MyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
