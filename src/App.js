b2421faa34f4524e82b429c204ac3ddbf73d7280
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-15 17:40:41 +0900

    2023-01-15) save point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Subject from "./components/Subject";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
@@ -22,10 +24,11 @@ class App extends Component {
 
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read') {
       var i = 0;
       while(i < this.state.contents.length) {
@@ -37,6 +40,9 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>
     }
     console.log(this);
 
@@ -66,7 +72,12 @@ class App extends Component {
             });
           }.bind(this)}
           data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+          <Control onChangeMode={function(_mode) {
+            this.setState({
+              mode: _mode
+            });
+          }.bind(this)}></Control>
+          {_article}
       </div>
     );
   }
