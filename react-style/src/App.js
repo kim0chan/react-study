b3fb41db9d03155f461a55338c3907d657eed516
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-25 17:05:36 +0900

    2023-01-25) function vs class style comparison

--- react-style/src/App.js
+++ react-style/src/App.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+  console.log('numberState', numberState);
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>props number: {number}</p>
+      <input type="button" value="random_func" onClick={
+        function() {
+          setNumber(Math.random());
+        }
+      }></input>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
+  state = {
+    number: this.props.initNumber
+  }
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>props number: {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function() {
+            this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
       </div>
     );
   }
