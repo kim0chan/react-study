f8031d3cd241ca8674cc490e7e47231c5f5ff62b
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-26 13:33:32 +0900

    2023-01-26) studying lifecycle API

--- react-style/note.md
+++ react-style/note.md
@@ -8,3 +8,21 @@ state 값이 바뀌면 클래스 컴포넌트의 `render` 메서드가 호출되
 리액트 16.8 버전부터 훅이라는 기능이 도입되었다.  
 리액트 기본 내장 훅이 있고, 사용자 정의 훅을 만들어서 사용할 수도 있다. [공식 문서](https://ko.reactjs.org/docs/hooks-intro.html)  
 리액트에서 제공하는 훅의 이름은 `useState`이다. `useState`는 배열을 반환한다.  
+배열의 첫 번째 원소는 **전달된 state 값**이고, 두 번째 원소는 **상태를 바꾸는 함수**이다.
+```javascript
+var numberState = useState(props.initNumber);
+var number = numberState[0];
+var setNumber = numberState[1];
+
+setNumber(Math.random());
+```
+다음과 같이 사용했다. 혹은 간단하게  
+```js
+var [number, setNumber] = useState(props.initNumber);
+```
+이렇게 쓰는 것이 더 선호된다.  
+
+---
+## 라이프사이클 구현
+이제 함수 방식에서도 state를 사용할 수 있게 되었다.  
+다음은 함수 방식에서 라이프사이클을 다루는 법을 알아보자.
--- react-style/src/App.js
+++ react-style/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -11,36 +11,58 @@ function App() {
   );
 }
 
+var funcStyle = 'color:blue';
+var funcId = 0;
 function FuncComp(props) {
-  var numberState = useState(props.initNumber);
-  var number = numberState[0];
-  var setNumber = numberState[1];
-  console.log('numberState', numberState);
+  var [number, setNumber] = useState(props.initNumber);
+  var [_date, setDate] = useState((new Date()).toString());
+
+  useEffect(function() {
+    console.log('%cfunc => useEffect ' + (++funcId), funcStyle);
+    document.title = number + ' : ' + _date;
+  })
+
+  console.log('%cfunc => render '+(++funcId), funcStyle);
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>props number: {number}</p>
+      <p>Date: {_date}</p>
       <input type="button" value="random_func" onClick={
         function() {
           setNumber(Math.random());
         }
       }></input>
+      <input type="button" value="date_now" onClick={
+        function() {
+          setDate(Date());
+        }
+      }></input>
+      
     </div>
   );
 }
 
 class ClassComp extends React.Component{
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
         <p>props number: {this.state.number}</p>
+        <p>date: {this.state.date}</p>
         <input type="button" value="random" onClick={
           function() {
-            this.setState({number:Math.random()})
+            this.setState({number:Math.random()});
+          }.bind(this)
+        }></input>
+
+        <input type="button" value="date now!" onClick={
+          function() {
+            this.setState({date:(new Date()).toString()});
           }.bind(this)
         }></input>
       </div>
