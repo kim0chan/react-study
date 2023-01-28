8c84a9610958438c5a34e3a1de943c14066c7aa7
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-01-28 23:29:44 +0900

    2023-01-28)

--- react-style/src/App.js
+++ react-style/src/App.js
@@ -2,11 +2,21 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  var [funcShow, setFuncShow] = useState(true);
+  var [classShow, setClassShow] = useState(true);
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input type="button" value="remove func" onClick={
+        function() {
+          setFuncShow(false);
+        }}></input>
+      <input type="button" value="remove class" onClick={
+        function() {
+          setClassShow(false);
+        }}></input>
+        {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}
+        {classShow ? <ClassComp initNumber={2}></ClassComp> : null}
     </div>
   );
 }
