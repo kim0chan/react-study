13acfc6b00bb6be82d92c2ff5abd16534adb93a9
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-04-01 22:39:11 +0900

    2023-04-01) implemented now loading

--- react-ajax/src/App.js
+++ react-ajax/src/App.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react';
 
+class NowLoading extends Component{
+  render() {
+    return(
+      <div>Now Loading....</div>
+    );
+  }
+}
+
 class Nav extends Component {
-  state = {
-    list: []
-  }
-  componentDidMount() {
-    fetch('list.json').then(function(result) {
-      return result.json();   // json 파일의 데이터를 javascript 객체로 변환
-    }).then(function(json) {
-      console.log(json);
-      this.setState({list:json});
-    }.bind(this));
-  }
+  // state = {
+  //   list: []
+  // }
+  // componentDidMount() {
+  //   fetch('list.json').then(function(result) {
+  //     return result.json();   // json 파일의 데이터를 javascript 객체로 변환
+  //   }).then(function(json) {
+  //     console.log(json);
+  //     this.setState({list:json});
+  //   }.bind(this));
+  // }
 
   render() {
     var listTag = [];
-    for(var i = 0; i < this.state.list.length; i++) {
-      var li = this.state.list[i];
+    for(var i = 0; i < this.props.list.length; i++) {
+      var li = this.props.list[i];
       listTag.push(
         <li key={li.id}>
           <a href={li.id} data-id={li.id} onClick={function(e) {
@@ -51,29 +59,71 @@ class Article extends Component {
 
 class App extends Component {
   state = {
-    article:{title: 'Welcome', desc: 'Hello, React & Ajax'}
+    article: {
+      item: {title:'Welcome', desc:'Hello, React & Ajax'},
+      isLoading: false
+    },
+    list: {
+      items: [],
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    var newList = Object.assign({}, this.state.list, {isLoading:true});
+    this.setState({list:newList});
+    fetch('list.json').then(function(result) {
+      return result.json();
+    }).then(function(json) {
+      console.log(json);
+      this.setState({list: {
+        items: json,
+        isLoading: false
+      }
+    });
+    }.bind(this));
   }
 
   render() {
+    var NavTag = null;
+    if(this.state.list.isLoading) {
+      NavTag = <NowLoading></NowLoading>
+    } else {
+      NavTag = <Nav list={this.state.list.items} onClick={function(id) {
+        var newArticle = Object.assign({}, this.state.article, {isLoading:true});
+        this.setState({article:newArticle});
+        fetch(id+'.json')
+        .then(function(result) {
+          return result.json();
+        })
+        .then(function(json) {
+          this.setState({
+            article:{
+              item:{
+                title: json.title,
+                desc: json.desc
+              },
+              isLoading: false
+            }
+          })
+        }.bind(this))
+      }.bind(this)}></Nav>
+    }
+
+    var ArticleTag = null;
+    if(this.state.article.isLoading) {
+      ArticleTag = <NowLoading></NowLoading>
+    } else {
+      ArticleTag = <Article title={this.state.article.item.title} dec={this.state.article.item.desc}></Article>
+    }
+
     return(
       <div className="App">
         <h1>WEB</h1>
-        <Nav onClick={function(id) {
-          fetch(id + '.json')
-          .then(function(result) {
-            return result.json();
-          })
-          .then(function(json) {
-            this.setState({
-              article:{
-                title:json.title,
-                desc:json.desc
-              }
-            });
-          }.bind(this));
-        }.bind(this)}></Nav>
-        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+          {NavTag}
+          {ArticleTag}
       </div>
+      
     );
   }
 }
